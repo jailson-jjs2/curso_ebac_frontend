d5162527be533591105a4597aca96b02e0a2cc92
Jailson Joventino <jjs2@hotmail.com>
2023-04-05 10:41:37 -0300

    Exercicio M10

---
+++ exercicioModulo10/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  $('#telefone').mask('(00) 00000-0000')
+  $('#cpf').mask('000.000.000-91')
+  $('#cep').mask('00000-000')
+
+  $('form').validate( {
+    rules: {
+      nome: {
+        required: true,
+        nomeCompleto: true // REGRA PARA NOME COMPLETO
+      },
+      email: {
+        required: true,
+        email:true
+      },
+      telefone: {
+        required: true
+      },
+      cpf: {
+        required: true
+      },
+    },
+    messages: {
+      nome: {
+        nomeCompleto: 'Por favor, digite o nome completo'
+      }
+    }
+  })
+
+  // REGRA PARA NOME COMPLETO
+  $.validator.addMethod('nomeCompleto', function(valor) {
+    const nomeComArray = valor.split(' ');
+    return nomeComArray.length >= 2;
+  });
+
+});
