dd649d4e3b9217be7245f0e839bfe4495ef92704
Jailson Joventino <jjs2@hotmail.com>
2023-04-05 10:44:35 -0300

    Exercicio M10 V final

--- exercicioModulo10/js/main.js
+++ exercicioModulo10/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $('#telefone').mask('(00) 00000-0000')
-  $('#cpf').mask('000.000.000-91')
+  $('#cpf').mask('000.000.000-00')
   $('#cep').mask('00000-000')
 
   $('form').validate( {
