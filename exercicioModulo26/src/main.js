f34f0dc987f571b9ecd7080458e49622845528ce
Jailson Joventino <jjs2@hotmail.com>
2023-05-15 18:28:32 -0300

    Concluido Exercicio M26

---
+++ exercicioModulo26/src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//FUNÇÃO MUILTIPLICAR
+const multiplicar = (num1, num2) => num1 * num2;
+// FUNÇÃO OLA
+const funcaoOla = (nome) => `Olá, ${nome}`;
---
+++ exercicioModulo26/src/main.ts
@@ -0,0 +1,5 @@
+//FUNÇÃO MUILTIPLICAR
+const multiplicar = (num1: number, num2: number): number => num1 * num2;
+
+// FUNÇÃO OLA
+const funcaoOla = (nome: string): string => `Olá, ${nome}`;
